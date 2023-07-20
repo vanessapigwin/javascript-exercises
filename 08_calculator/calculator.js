fdd43fbca56d7367bb59c03b1506941e570e4529
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2023-07-20 15:03:41 +0800

    initial commit, 1-8 ok

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(...args) {
+  let total = 0;
+	args.forEach((arg) => total += arg);
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args[0] - args[1];
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	let total = 0;
+	args.forEach((arg) => total += arg);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let product = 1;
+  args.forEach((arg)=>product *= arg);
+  return product;
 };
 
-const power = function() {
-	
-};
+const power = (m, n) => m**n;
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let product = 1
+  for (i=0; i <= n; i++) {
+    product *= (i - 1 > 0) ? (i) : 1; 
+  }
+  return product;
 };
 
 // Do not edit below this line
