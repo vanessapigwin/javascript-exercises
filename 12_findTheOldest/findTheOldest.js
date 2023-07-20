b5642f1c8661091d55d764572f770dce582cf2e0
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2023-07-20 21:53:01 +0800

    finished all modules

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(data) {
+    let maxAge = 0;
+    let oldest = '';
 
+    for (person of data) {
+        person.yearOfDeath = !(person.yearOfDeath) ? new Date().getFullYear() : person.yearOfDeath;
+        age = person.yearOfDeath - person.yearOfBirth;
+
+        if (age > maxAge) {
+            maxAge = age;
+            oldest = person;
+        }
+    }
+    return oldest;
+    
 };
 
 // Do not edit below this line
